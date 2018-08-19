f0939442e8c362572c9ccef00e5d2cb3185d12d3
PengInGitHub <felixdeutschland@hotmail.com>
2018-08-19 15:11:49 +0200

    add more sql query

--- pandasql.py
+++ pandasql.py
@@ -8,6 +8,11 @@ def select_first_50(filename):
 
  q = "select registrar, enrolment_agency from aadhaar limit 5"
  q_complex = "select gender, district, sum(aadhaar_generated) from aadhaar where age>50 group by gender, district"
+ q_weather = "select count(*) from weather_data where cast( rain as integer) = 1"
+ q_maxtemp = "select fog, max(max_temp) from weather_data group by fog"
+ q_meantemp = "select avg(meantemp) from weather_data where cast(strftime('%w', date) as integer)=0 or cast(trftime('%w', date) as integer)=6"
+ q_rain = "select avg(mintempi) from weather_data where rain=1 and mintempi>55"
+
  aadhaar_sql = pandasql.sqldf(q.lower())
  return aadhaar_sql
     
