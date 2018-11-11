407d3757dfc85256d3037272d3d0bf35bb77dc4d
PengInGitHub <felixdeutschland@hotmail.com>
2018-11-11 21:16:20 +0100

    add probability and algebra

--- DataScienceFromScratch/s.py
+++ DataScienceFromScratch/s.py
@@ -297,6 +297,73 @@ plt.xlabel('test 1 grade')
 plt.ylabel('test 2 grade')
 plt.show()
 
+# histogram
+import math
+
+def bucketize(point, bucket_size):
+    return bucket_size * math.floor(point/bucket_size)
+
+def make_histogram(points, bucket_size):
+    # bucketize the points and count how many in each bucket
+    return Counter(bucketize(point, bucket_size) for point in points)
+
+
+def plot_histogram(points, bucket_size, title=''):
+    histogram = make_histogram(points, bucket_size)
+    plt.bar(histogram.keys(), histogram.values(),width=bucket_size)
+    plt.title(title)
+    plt.show()
+
+
+def normal_cdf(x, mu=0,sigma=1):
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
+def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
+    """find approximate inverse using binary search"""
+    # if not standard, compute standard and rescale
+    if mu != 0 or sigma != 1:
+        return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+    low_z, low_p = -10.0, 0 # normal_cdf(-10) is (very close to) 0
+    hi_z, hi_p = 10.0, 1 # normal_cdf(10) is (very close to) 1
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z + hi_z) / 2 # consider the midpoint
+        mid_p = normal_cdf(mid_z) # and the cdf's value there
+        if mid_p < p:
+        # midpoint is still too low, search above it
+            low_z, low_p = mid_z, mid_p
+        elif mid_p > p:
+        # midpoint is still too high, search below it
+            hi_z, hi_p = mid_z, mid_p
+        else:
+            break
+    return mid_z
+
+
+random.seed(0)
+# uniform between -100 t0 100
+uniform = [200 * random.random() - 100 for _ in range(100000)]
+
+normal = [57 * inverse_normal_cdf(random.random())
+         for _ in range(100000)]
+
+
+# uniform and normal have very diff distributions
+plot_histogram(uniform, 10, 'Uniform Histogram')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
