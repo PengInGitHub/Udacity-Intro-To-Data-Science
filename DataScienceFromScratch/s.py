b5e2e3b2b21fc54fca3eb513fb8ac1f6e651b9ab
PengInGitHub <felixdeutschland@hotmail.com>
2018-11-14 23:00:23 +0100

    add SQL

--- DataScienceFromScratch/s.py
+++ DataScienceFromScratch/s.py
@@ -456,8 +456,25 @@ def user_based_suggestions(user_id, include_current_interests=False):
             suggestions[interest] += similarity 
     
     # convert them to a sorted list
+    suggestions = sorted(suggestions.items(),
+                         key=lambda(_, weight):weight,
+                         reverse = True)
     
-
+    
+# SQL 
+class Table:
+    def __init__(self, columns):
+        self.columns = columns
+        self.rows = []
+    def __repr__(self):
+        """pretty representation of the table: columns then rows"""
+        return str(self.columns) + "\n" + "\n".join(map(str, self.rows))
+    
+    def insert(self, row_values):
+        if len(row_values) != len(self.columns):
+            raise TypeError("wrong number of elements")
+        row_dict = dict(zip(self.columns, row_values))
+        self.rows.append(row_dict)
 
 
 
